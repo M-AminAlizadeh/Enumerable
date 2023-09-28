d614931f08aa73a0a6b9214b1a2e6017c0f674b9
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-28 18:44:40 +0500

    Create all? method in MyEnumerable module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,3 +1,10 @@
-def MyEnumerable
-  
+module MyEnumerable
+  def all?
+    if block_given?
+      each { |item| false unless yield(item) }
+    else
+      each { |item| false unless item }
+    end
+    true
+  end
 end
